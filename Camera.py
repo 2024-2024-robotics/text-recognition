cddbc483907ba83f9a8bcd16ce0310cd6b07b021
allsbrook0928 <ntallsbrook@gmail.com>
2023-11-10 12:41:05 -0500

    program can be run from one file now!! (Run.py)

--- Camera.py
+++ Camera.py
@@ -3,13 +3,15 @@ from PIL import Image
 
 camera = cv2.VideoCapture(0)
 
-while True:
-    _, image = camera.read()
-    cv2.imshow('Text Detection', image)
+def init():
 
-    if cv2.waitKey(1) & 0xFF == ord('s'):
-        cv2.imwrite('image.png', image)
-        break
+    while True:
+        _, image = camera.read()
+        cv2.imshow('Text Detection', image)
+
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            cv2.imwrite('image.png', image)
+            break
         
-camera.release()
-cv2.destroyAllWindows()
+    camera.release()
+    cv2.destroyAllWindows()
--- PrintText.py
+++ PrintText.py
@@ -2,24 +2,26 @@ import cv2
 from PIL import Image
 from pytesseract import pytesseract
 
-def tesseract():
-    path_to_tesseract = "/usr/bin/tesseract"
-    image_path = "processed image.png"
-    pytesseract.tesseract_cmd = path_to_tesseract
-    text = pytesseract.image_to_string(Image.open(image_path))
+def init():
 
-    # text = list(text)
+    def tesseract():
+        path_to_tesseract = "/usr/bin/tesseract"
+        image_path = "processed image.png"
+        pytesseract.tesseract_cmd = path_to_tesseract
+        text = pytesseract.image_to_string(Image.open(image_path))
 
-    if '/' in text: 
-        text = text.replace('/', '7')
-    else:
-        pass
+        # text = list(text)
+
+        if '/' in text: 
+            text = text.replace('/', '7')
+        else:
+            pass
     
-    text = list(text)
-    del text[3:len(text)]
+        text = list(text)
+        del text[3:len(text)]
 
-    coordinate_string = ''.join(text) 
+        coordinate_string = ''.join(text) 
 
-    print(coordinate_string)
+        print(coordinate_string)
 
-tesseract()
+    tesseract()
